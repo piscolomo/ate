85cb472ab11d58472655e4c059a6e44cf502908d
Julio Lopez <ljuliom@gmail.com>
2015-03-29 00:01:43 -0500

    added methods in ate

--- lib/ate.rb
+++ lib/ate.rb
@@ -1,33 +1,38 @@
 class Ate
-
-  def self.parse(template, vars = {})
-    lines = template.end_with?(".ate") ? File.read(template) : template
-    lines = lines.split("\n")
-
-    proc = "Proc.new do \n "
-    proc << "output = \"\" \n "
-
-    vars.each do |x, y|
-      value = y.is_a?(String) ? "\"#{y}\"" : y
-      proc << "#{x} = #{value}\n"
+  class << self
+    def parse(template, vars = {})
+      lines = template.end_with?(".ate") ? File.read(template) : template
+      lines = lines.split("\n")
+      @parsed = eval(build_proc(lines, vars))
+      self
     end
 
-    lines.each do |line|
-      if line =~ /^\s*(%)(.*?)$/
-        proc << "#{line.gsub(/^\s*%(.*?)$/, '\1') } \n"
-      else
-        proc << "output << \"#{line.gsub(/\{\{([^\r\n\{]*)\}\}/, '#{\1}') }\n\" \n "
+    def build_proc(lines, vars)
+      @proc = "Proc.new do \n output = \"\" \n "
+      declaring_local_variables(vars)
+      parsing_lines(lines)
+      @proc << "output \n end"
+    end
+
+    def declaring_local_variables(vars)
+      vars.each do |x, y|
+        value = y.is_a?(String) ? "\"#{y}\"" : y
+        @proc << "#{x} = #{value}\n"
       end
     end
 
-    proc << "output \n"
-    proc << "end"
+    def parsing_lines(lines)
+      lines.each do |line|
+        if line =~ /^\s*(%)(.*?)$/
+          @proc << "#{line.gsub(/^\s*%(.*?)$/, '\1') } \n"
+        else
+          @proc << "output << \"#{line.gsub(/\{\{([^\r\n\{]*)\}\}/, '#{\1}') }\n\" \n "
+        end
+      end
+    end
 
-    @parsed = eval(proc)
-    self
-  end
-
-  def self.render
-    @parsed.call
+    def render
+      @parsed.call
+    end
   end
 end
