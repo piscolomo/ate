bc89097d9c4a5a5bc1196ac4d6d6143b1cfba96a
Julio Lopez <ljuliom@gmail.com>
2015-03-28 19:09:53 -0500

    ate can accept template as string or a file

--- lib/ate.rb
+++ lib/ate.rb
@@ -1,5 +1,23 @@
 class Ate
-  def self.parse
-    "gogo ate-callao!"
+
+  def self.parse(template, vars = "")
+    lines = template.end_with?(".ate") ? File.read(template) : template
+    lines = lines.split("\n")
+
+    proc = "Proc.new do |#{vars}| \n "
+    proc << "output = \"\" \n "
+
+    lines.each do |line|
+      if line =~ /^\s*(%)(.*?)$/
+        proc << "#{line.gsub(/^\s*%(.*?)$/, '\1') } \n"
+      else
+        proc << "output << \"#{line.gsub(/\{\{([^\r\n\{]*)\}\}/, '#{\1}') }\n\" \n "
+      end
+    end
+
+    proc << "output \n"
+    proc << "end"
+
+    eval(proc)
   end
 end
--- test/test.rb
+++ test/test.rb
@@ -1,8 +1,16 @@
 require File.expand_path("../lib/ate", File.dirname(__FILE__))
 
 scope do
-  test "just veryfing" do
-    content = Ate.parse
-    assert_equal "gogo ate-callao!", content
+  test "returning same number of empty lines" do
+    example = Ate.parse("\n\n \n")
+    assert_equal "\n\n \n", example.call
+  end
+  test "returning same number of empty lines with mixed code" do
+    example = Ate.parse("\n% true\n\n% false\n\n")
+    assert_equal "\n\n", example.call
+  end
+
+  test "looking a file" do
+    assert_equal "Beetlejuice\nBeetlejuice\nBeetlejuice\n", Ate.parse("% 3.times do\nBeetlejuice\n% end").call
   end
 end
